999ecbc12fbd65f436b5fc216e192f1278dcde4d
Judith Verstegen <judithverstegen@gmail.com>
2019-06-04 15:03:59 +0200

    commit local change to allow merge

--- parameters.py
+++ parameters.py
@@ -8,7 +8,7 @@ def getNrTimesteps():
 
   e.g. 2005 to 2030 is 26 time steps."""
 
-  timesteps = 22
+  timesteps = 2
   return timesteps
 
 def getNrSamples():
@@ -16,7 +16,7 @@ def getNrSamples():
 
   If Monte Carlo isn't required fill in 1; no statistics will be calculated."""
   
-  samples = 100
+  samples = 2
   return samples
   
 def getCovarOn():
