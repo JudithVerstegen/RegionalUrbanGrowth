0c03b1981f25f5a7ddb4d9e5d829520b6dc97def
katarzynagoch <45998168+katarzynagoch@users.noreply.github.com>
2019-10-29 22:51:51 +0100

    Update transform_save_data.py

--- transform_save_data.py
+++ transform_save_data.py
@@ -1,3 +1,5 @@
+# RESOLVE: RuntimeWarning: More than 20 figures have been opened. Figures created through the pyplot interface (`matplotlib.pyplot.figure`) are retained until explicitly closed and may consume too much memory.
+
 # -*- coding: cp1252 -*-
 import pickle
 #from collections import deque
