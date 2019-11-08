20692da913592e18896f104dd9f84e853887a9f0
katarzynagoch <45998168+katarzynagoch@users.noreply.github.com>
2019-11-08 13:16:52 +0100

    array for storing RMSE created

--- calibrate.py
+++ calibrate.py
@@ -11,8 +11,8 @@ import matplotlib.pyplot as plt
 #### Script to find calibrate LU model
 
 # Get metrics
-#variables = parameters.getSumStats() 
-variables = ['np'] # FOR TESTING
+#metricList = parameters.getSumStats() 
+metricList = ['np'] # FOR TESTING
 
 # Get the number of parameter iterations and number of time step defined in the parameter.py script
 nrOfTimesteps=parameters.getNrTimesteps()
@@ -23,7 +23,7 @@ timeSteps=range(1,nrOfTimesteps+1,1)
 
 # Get the observed time steps. Time steps relate to the year of the CLC data, where 1990 was time step 0.
 obsSampleNumbers = [1] #range(1,20+1,1) <- for stochastic model
-obsTimeSteps = [10,16] # for a whole run should be amended # year 2000 and 2006. maybe should go to parameters?
+obsTimeSteps = [10] # for a whole run should be amended # year 2000 and 2006. maybe should go to parameters?
 
 # Path to the folder with the metrics npy arrays stored
 country = parameters.getCountryName()
@@ -34,7 +34,17 @@ arrayFolder = os.path.join(os.getcwd(),'results',country,'metrics')
 ### FUNCTIONS ###
 #################
 
-
+# Create a list of parametr configurations
+def getParameterConfigurations(metricsArray):
+  allParameters = []
+  for i in range(0,len(metricsArray[0])):
+    allParameters.append(metricsArray[0][i][0])
+  print('Number of parameter configurations: ',metricsArray.shape[1])
+  return allParameters
+def subsetParameters(metricsArray):
+  
+  print(metricsArray[:,0])
+  return 'done'
 
 ###########################
 ### CALIBRATE THE MODEL ###
@@ -48,46 +58,45 @@ of the selected metric for each zone for each set of parameters seperately")
 # Calibration of the modell will be based on finding
 # minimum root-mean-square error between the metrics modelled and observed.
 
+for aVariable in metricList:
+  zonesModelled = np.load(os.path.join("results", country, 'metrics', aVariable + '.npy'))
+  
+  # 1. Create a list containing possible paramter configurations
+  allParameters = getParameterConfigurations(zonesModelled)
+  noParameterConfigurations = zonesModelled.shape[1]
+  
+  # 2. Create an array to store the difference between modelled and observed metrics value
+  differenceArray = np.zeros((len(obsTimeSteps),noParameterConfigurations,16,1)) # change this!!!!!
+  #print(differenceArray)
+
+  # 2. Store the metric value for selected timestep
+  for obsTimeStep in obsTimeSteps:
+    rowIndex = 0
+    for p in range(0,noParameterConfigurations):
+      differenceArray[rowIndex,p] = zonesModelled[obsTimeStep-1,p][1]
+    rowIndex = rowIndex+1
+  print(differenceArray)
+                                                  
+  
+
+
 # 1. Calculate Mean Squared Error
-numberOfDataPoints = 2 # number of observations. Here: 2000 and 2006
+numberOfDataPoints = 1 # number of observations. Here: 2000
 
 # Get the modelled values corresponding to the observed values
-for aVariable in variables:
-  zonesModelled = np.load(os.path.join("results", country, 'metrics', aVariable + '.npy'))
-  print(zonesModelled[0])
+
+'''print(zonesModelled)
   metricValueArray = [] # [metric name, parameter set, timestep, zone, metric value]
   metricsArray = [] # array to store matricValueArray[] for each zone for each timestep for each metric
   
-
   for timeStep in timeSteps: # Loop data for each time step
-    
     for zone in range(0, len(zonesModelled[timeStep-1][0][1])): # Loop array to extraxt data for each zone
-
       for i in range(0,len(zonesModelled[timeStep-1])): # get the metric for the time step in given zone
         for obsTimeStep in obsTimeSteps:
           if timeStep == obsTimeStep:
             metricValueArray = [aVariable,zonesModelled[timeStep-1][i][0],timeStep,zone,zonesModelled[timeStep-1][i][1][zone][0]]
             metricsArray.append(metricValueArray)
-            metricValueArray = []
-
-# Create a list of parametr configurations
-allParameters = []
-for i in range(0,len(metricsArray)):
-  allParameters.append(metricsArray[i][1])
-
-# Convert array to list
-allParameters = np.array(allParameters).tolist()
-# Find duplicates
-paramList = []
-for elem in allParameters:
-    if elem not in paramList:
-        paramList.append(elem)
-print(paramList)
-
-for paramSet in paramList:
-  print(paramSet)
-  if (metricsArray[1][1] == paramSet).all():
-    print('.')
+            metricValueArray = []'''
 
 
 
